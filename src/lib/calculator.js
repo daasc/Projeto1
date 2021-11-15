523d671f066411080674932cc17ee6c99712f271
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2021-11-15 19:44:36 -0300

    add calculator file and your test

---
+++ src/lib/calculator.js
@@ -0,0 +1,11 @@
+const sum = (num1, num2)  => { 
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+
+  if(Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw new Error('Please check your input');
+  }
+  return num1 + num2;
+}
+
+module.exports = { sum }
---
+++ src/lib/calculator.spec.js
@@ -0,0 +1,18 @@
+const { sum } = require('./calculator');
+
+it('should sum 2 and 2 the result must be 4', () => {
+  expect(sum(2, 4)).toBe(6);
+});
+
+it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+  expect(sum("2", 4)).toBe(6);
+});
+
+it('should throw an error if what is provided to the methos cannot be summed', () => {
+  
+  expect(() => {
+    sum('', 1)
+  }).toThrowError();
+
+  
+});
