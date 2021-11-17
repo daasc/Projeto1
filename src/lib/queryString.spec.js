5c2e8798c0984bbe07de1b2579ff448dec123bf9
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2021-11-17 18:31:07 -0300

    master fix: add new test

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,25 +1,24 @@
-const {queryString, parse} = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
     const obj = {
       name: 'Paulo',
-      profession: 'developer'
+      profession: 'developer',
     };
 
     expect(queryString(obj)).toBe('name=Paulo&profession=developer');
-
   });
   it('should throw an erro when an object is passed as value', () => {
     const obj = {
       name: 'Paulo',
       abilities: {
         first: 'developer',
-      } 
+      },
     };
     expect(() => {
       queryString(obj);
-    }).toThrowError()
+    }).toThrowError();
   });
 });
 
@@ -28,14 +27,22 @@ describe('Query string to object', () => {
     const qs = 'name=Paulo&profession=developer';
     expect(parse(qs)).toEqual({
       name: 'Paulo',
-      profession: 'developer'
-    })
+      profession: 'developer',
+    });
   });
 
   it('should convert a query string of a single key-value to object', () => {
     const qs = 'name=Paulo';
     expect(parse(qs)).toEqual({
-      name: 'Paulo'
+      name: 'Paulo',
+    });
+  });
+
+  it('should convert a query string to an object takiing care of comma separated values', () => {
+    const qs = 'name=Paulo&abilities=JS,TDD';
+    expect(parse(qs)).toEqual({
+      name: 'Paulo',
+      abilities: ['JS', 'TDD']
     })
   });
 });
