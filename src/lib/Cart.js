756e53b68e2d2cb9b792364f420b609563a2b61f
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2021-12-09 12:33:33 -0300

    add remove

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,16 +1,22 @@
-import find  from "lodash/find";
-import remove from "lodash/remove";
+import find from 'lodash/find';
+import remove from 'lodash/remove';
 export default class Cart {
-  items = []
+  items = [];
   add(item) {
-    const itemToFind = {product: item.product}
+    const itemToFind = { product: item.product };
 
     if (find(this.items, itemToFind)) {
-      remove(this.items, itemToFind)
+      remove(this.items, itemToFind);
     }
-    this.items.push(item)
+    this.items.push(item);
+  }
+  remove(product) {
+    remove(this.items, { product });
   }
   getTotal() {
-    return this.items.reduce((acc, item) => acc + item.quantity * item.product.price, 0);
+    return this.items.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0,
+    );
   }
 }
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -4,7 +4,11 @@ describe('Carts', () => {
   let cart
   let product = {
     title: 'Adidas',
-    price: 35388 // 353.88
+    price: 35388 
+  }
+  let product2 = {
+    title: 'nike',
+    price: 41872 
   }
   beforeEach(() => {
     cart = new Cart();    
@@ -37,5 +41,20 @@ describe('Carts', () => {
 
     expect(cart.getTotal()).toEqual(35388)
     
+  });
+
+  it('should update total when a product gets inclued and then removed', () => {
+    cart.add({
+      product,
+      quantity: 2
+    })
+    cart.add({
+      product: product2,
+      quantity: 1
+    })
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(41872);
   });
 });
